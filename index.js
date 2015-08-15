8871a64756124de17b7bef1b49427bb4a7a42cfc
Nick Matenaar <nick@prismatik.com.au>
2015-08-15 18:05:46 +1000

    Clean up a little

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 var path = require('path');
 var request = require('request');
-var pkg = require( path.join(__dirname, 'package.json') );
-
 var WebSocketClient  = require('websocket').client;
-
 var program = require('commander');
+
+var pkg = require( path.join(__dirname, 'package.json') );
 
 program
 	.version(pkg.version)
@@ -14,48 +13,50 @@ program
   
 var client = new WebSocketClient();
   
-var RTM_URL = "https://slack.com/api/rtm.start";
-var REACTIONS_URL = "https://slack.com/api/reactions.get";
-var POST_MESSAGE_URL = "https://slack.com/api/chat.postMessage";
+var SLACK_API = "https://slack.com/api";
+var GITHUB_API = "https://api.github.com";
   
 console.log('Connecting to slack...')
-request.post(RTM_URL, {form: {token: program.key}}, function(err, res, body) {
+request.post(SLACK_API + '/rtm.start', {form: {token: program.key}}, function(err, res, body) {
   var wsURL = JSON.parse(body).url;
+  
   client.connect(wsURL);
-})
+});
 
 client.on('connect', function(conn) {
   console.log('connected');
   
   conn.on('message', function(message) {
-    var body = JSON.parse(message.utf8Data);
-    if (body.type !== 'reaction_added') return;
+    message = JSON.parse(message.utf8Data);
+    if (message.type !== 'reaction_added') return;
+    
     var reactionOpts = {
       token: program.key,
-      timestamp: body.item.ts,
-      channel: body.item.channel
+      timestamp: message.item.ts,
+      channel: message.item.channel
     };
-    request.post(REACTIONS_URL, {form: reactionOpts}, function(err, res, postBody) {
-      var text = JSON.parse(postBody).message.text;
+    
+    request.post(SLACK_API + '/reactions.get', {form: reactionOpts}, function(err, res, body) {
+      var text = JSON.parse(message).message.text;
+      
       gistify(text, function(url) {
         var messageOpts = {
           token: program.key,
-          channel: body.item.channel,
+          channel: message.item.channel,
           'as_user': true,
           text: 'Gist: ' + url
         };
-        request.post(POST_MESSAGE_URL, {form: messageOpts}, function(err, res, postBody) {
-          postBody = JSON.parse(postBody);
-          if (postBody.ok) console.log('Successfully created gist, ' + url)
-        })
-      })
-    })
+        
+        request.post(SLACK_API + '/chat.postMessage', {form: messageOpts}, function(err, res, body) {
+          body = JSON.parse(body);
+          if (body.ok) console.log('Successfully created gist, ' + url);
+        });
+      });
+    });
   });
 });
 
 var gistify = function(text, cb) {
-  var url = "https://api.github.com/gists";
-  
   var form = {
     pubic: true,
     files: {
@@ -67,14 +68,14 @@ var gistify = function(text, cb) {
   
   var requestOpts = {
     method: 'POST',
-    url: url,
+    url: GITHUB_API + '/gists',
     headers: {
       'User-Agent': 'gist-this'
     },
     json: form
-  }
+  };
   
   request(requestOpts, function(err, res, body) {
     cb(body['html_url']);
-  })
+  });
 };
